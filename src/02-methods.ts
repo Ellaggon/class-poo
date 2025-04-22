c7b2c95eac610a9a5ee9bf6293a1614abfc44fe1
ellaggon <ellaggon@gmail.com>
2025-04-21 23:39:55 -0400

    feat(method): create method into MyDate class

---
+++ src/02-methods.ts
@@ -0,0 +1,33 @@
+
+
+export class MyDate {
+    year: number
+    month: number
+    day: number
+
+    constructor(year: number, month: number, day: number) {
+        this.year = year
+        this.month = month
+        this.day = day
+    }
+    printFormat() {
+        return `${this.day}/${this.month}/${this.year}`
+    }
+    add(amount: number, type: "days" | "months" | "years") {
+        if(type === "days"){
+            this.day += amount
+        }
+        if(type === "months"){
+            this.month += amount
+        }
+        if(type === "years"){
+            this.year += amount
+        }
+    }
+}
+
+const myDate = new MyDate(2021, 1, 9) 
+console.log("format: ", myDate.printFormat())
+myDate.add(10, "years")
+console.log("format later: ", myDate.printFormat())
+
